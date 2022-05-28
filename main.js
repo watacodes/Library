2225846b9fac4f80ba6e81193e79f71c62ec4ad5
Wata <bireleysaddiction@gmail.com>
2022-05-28 15:48:24 +0900

    Temporarily gcommenting out the displaybooksAddedByUser function and trying out the existing displayBooks function to replace it.

--- main.js
+++ main.js
@@ -2,28 +2,35 @@
 
 let myPersonalLibrary = [];
 
+// Adding books manually to see whether it's displayed correctly.
+
+// const newBook1 = new addMyBookToLibrary("Harry Potter", "J.K. Rowlings", "Fiction", "5");
+// const newBook2 = new addMyBookToLibrary("Atomic Habits", "James Clear", "Non-Fiction", "5");
+// const newBook3 = new addMyBookToLibrary("Grit", "Angela Duckworth", "Non-Fiction", "5");
+// const newBook4 = new addMyBookToLibrary("The Alchemist", "Paulo Coelho", "Fiction", "5");
+// const newBook5 = new addMyBookToLibrary("Homo Deus", "Yuval Noah Harari", "Non-Fiction", "5");
+// const newBook6 = new addMyBookToLibrary("1Q84", "Haruki Murakami", "Fiction", "5");
+// const newBook7 = new addMyBookToLibrary("Man's Search for Meaning", "Viktor Frankl", "Non-Fiction", "5");
+// const newBook8 = new addMyBookToLibrary("Moonwalking with Einstein", "Joshua Foer", "Non-Fiction", "5");
+
+const container = document.getElementById('container');
+
 // Making a constructor 
 
-function Book(title, author, genre, rating) {
+function Book(title, author, genre, rating, status) {
     this.title = title;
     this.author = author;
     this.genre = genre;
     this.rating = rating;
+    this.status = false;
 };
-
-const newBook1 = new addMyBookToLibrary("Harry Potter", "J.K. Rowlings", "Fiction", "5");
-const newBook2 = new addMyBookToLibrary("Atomic Habits", "James Clear", "Non-Fiction", "5");
-const newBook3 = new addMyBookToLibrary("Grit", "Angela Duckworth", "Non-Fiction", "5");
-const newBook4 = new addMyBookToLibrary("The Alchemist", "Paulo Coelho", "Fiction", "5");
-const newBook5 = new addMyBookToLibrary("Homo Deus", "Yuval Noah Harari", "Non-Fiction", "5");
-const newBook6 = new addMyBookToLibrary("1Q84", "Haruki Murakami", "Fiction", "5");
-const newBook7 = new addMyBookToLibrary("Man's Search for Meaning", "Viktor Frankl", "Non-Fiction", "5");
-const newBook8 = new addMyBookToLibrary("Moonwalking with Einstein", "Joshua Foer", "Non-Fiction", "5");
-const container = document.getElementById('container');
 
 function addMyBookToLibrary(title, author, genre, rating) {
     const book = new Book(title, author, genre, rating);
     myPersonalLibrary.push(book);
+    localStorage.setItem('Book', JSON.stringify(myPersonalLibrary));
+    displayBooks();
+    document.querySelector('form').reset();
 };
 
 const submitButton = document.getElementById('submit-btn');
@@ -39,20 +46,24 @@ submitButton.addEventListener('click', e => {
 
     if (!(userTitle == "" || userAuthor == "" || userRating == "")) {
         new addMyBookToLibrary(userTitle, userAuthor, userGenre, userRating);
-        console.log(`This is mylib : ${JSON.stringify(myPersonalLibrary)}`);
-        displayBooksAddedByUser();
     }
 });
 
 
 
 // Making a function to display books in the webpage, loop through the array,
-// get the book info, making divs to store them, appended those divs to the parent div #container
+// get the book info, make div elements to store them, append those divs to the parent div #container
 
 function displayBooks() {
+    // delete the existing cards
+
+    const existingCard = document.querySelectorAll('.card-body');
+
+    for (let i = 0; i < existingCard.length; i++) {
+        existingCard[i].remove();
+    }
+
     myPersonalLibrary.forEach((book) => {
-        
-        localStorage.setItem('Book', JSON.stringify(myPersonalLibrary));
         
         const newCard = document.createElement('div');
         newCard.className = 'card-body';
@@ -70,7 +81,6 @@ function displayBooks() {
     });
 };
 
-displayBooks();
 
 // Newly added function that adds new book data based on user input
 
@@ -79,24 +89,22 @@ displayBooks();
 // Can't store the info in the localStorage, created divs will be disappears
 // once I reload the page.
 
-function displayBooksAddedByUser() {
-    const lastItem = myPersonalLibrary[myPersonalLibrary.length - 1];
-    localStorage.setItem('Book', JSON.stringify(myPersonalLibrary));
-    console.log(myPersonalLibrary);
-    const createNewCard = document.createElement('div');
-    createNewCard.className = 'card-body';
+// function displayBooksAddedByUser() {
+//     const lastItem = myPersonalLibrary[myPersonalLibrary.length - 1];
+//     const createNewCard = document.createElement('div');
+//     createNewCard.className = 'card-body';
 
-    const newCardBody = `
-        <div class="card-body">
-            <span>Title: ${lastItem.title}</p>
-            <span>Author: ${lastItem.author}</p>
-            <span>Genre: ${lastItem.genre}</p>
-            <span>Rating: ${lastItem.rating}</p>
-        </div>
-    `
+//     const newCardBody = `
+//         <div class="card-body">
+//             <span>Title: ${lastItem.title}</p>
+//             <span>Author: ${lastItem.author}</p>
+//             <span>Genre: ${lastItem.genre}</p>
+//             <span>Rating: ${lastItem.rating}</p>
+//         </div>
+//     `
 
-    container.innerHTML += newCardBody;
-}
+//     container.innerHTML += newCardBody;
+// }
 
 
 
